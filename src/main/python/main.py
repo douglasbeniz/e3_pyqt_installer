c0e5e4e21fa114aa51b0b626185ef9b460be2240
Douglas Beniz <douglasbeniz@yahoo.com.br>
2019-01-13 17:18:46 +0100

    New button to clear log history, concluding the installtion procesures

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,15 +1,18 @@
 #from fbs_runtime.application_context import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QFileDialog, QLineEdit, QMessageBox
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QProcess, QEvent, Qt
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QProcess, QThread, QEvent, Qt
 from PyQt5.uic import loadUi
 
 from enum import Enum
 from pathlib import Path
-from time import sleep
+#from time import QThread.sleep
+from functools import partial
 
 import sys, os
 import requests
 import subprocess
+import shutil
+import time
 
 REQUIRE_ESS_REPO            = 'https://github.com/icshwi/require-ess/releases'
 REQUIRE_ESS_REPO_API_TAGS   = 'https://api.github.com/repos/icshwi/require-ess/tags'
@@ -82,10 +85,6 @@ class e3InstallerWindow(QMainWindow):
         if nextIndex > 0:
             self.pushPrevious.setEnabled(True)
 
-        # if nextIndex == 3:
-        #     print(self.requireVersion)
-        #     print(self.baseVersion)
-
         # process correspondent step
         self.processInstallationStep(nextIndex)
 
@@ -94,8 +93,8 @@ class e3InstallerWindow(QMainWindow):
         localDir = QFileDialog.getExistingDirectory(self, "EPICS installation via e3", self.defaultLocalDir, QFileDialog.ShowDirsOnly)
         if localDir:
             self.defaultLocalDir = localDir
-            self.localDir = localDir + '/e3/'
-            self.targetDir = self.targetDir.replace('//', '/')
+            self.localDir = localDir + '/e3'
+            self.localDir = self.localDir.replace('//', '/')
             self.textLocalDir.setText(self.localDir)
 
 
@@ -103,7 +102,7 @@ class e3InstallerWindow(QMainWindow):
         targetDir = QFileDialog.getExistingDirectory(self, "EPICS installation via e3", self.defaultTargetDir, QFileDialog.ShowDirsOnly)
         if targetDir:
             self.defaultTargetDir = targetDir
-            self.targetDir = targetDir + '/epics/'
+            self.targetDir = targetDir + '/epics'
             self.targetDir = self.targetDir.replace('//', '/')
             self.textTargetDir.setText(self.targetDir)
 
@@ -114,33 +113,137 @@ class e3InstallerWindow(QMainWindow):
     * ---------------------------------------------------------------------------
     """
     def pushInstallClicked(self):
-        # self.textLog.append("Local directory: " + self.localDir)
-        # self.textLog.append("Target directory: " + self.targetDir)
-        # -----------------------------------------------------------------------
-        cloneDir = ('%s/%s%s') % (self.localDir, 'e3-', self.baseVersion)
+        self.statusBar.showMessage('This will take a while, take a cup of tea or coffee, or go to work on something else...', 15000)
+        try:
+            # -----------------------------------------------------------------------
+            cloneDir = ('%s/%s%s') % (self.localDir, 'e3-', self.baseVersion)
+            cloneDir = cloneDir.replace('//', '/')
+            if not os.path.exists(self.localDir):
+                os.makedirs(self.localDir)
+            if (os.path.exists(cloneDir)):
+                choice = QMessageBox.question(self,
+                        "EPICS installation via e3",
+                        "It already exists a clone of e3 to desired EPICS version.\n\nWould you like to overwrite it on cancel the operation?",
+                        QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+                if choice == QMessageBox.Yes:
+                    shutil.rmtree(cloneDir)
+                    #pass
+                else:
+                    return 1
+            if not os.path.exists(self.targetDir):
+                os.makedirs(self.targetDir)
+
+            # -----------------------------------------------------------------------
+            # running processes in sequence...
+            # -----------------------------------------------------------------------
+            # cloning the e3 repo
+            self.processGitClone.start('git', ['clone', self.defaultRepo, cloneDir])
+            while not self.processGitClone.waitForFinished():
+                QThread.sleep(0.2)
+            self.processGitClone.close()
+            # -----------------------------------------------------------------------
+            # configure the e3 environment settings
+            configSetupParams = ['-t%s' % self.targetDir,              # target directory
+                '-b%s' % self.baseVersion.replace('R',''),             # EPICS base version
+                '-r%s' % self.requireVersion.replace('v',''),          # require version
+                '-y',                                                   # force to accept the cnofiguration as default (yes)
+                'setup']                                                # run the setup procedure
+            self.processConfigSetup.start(('%s/%s') % (cloneDir,'e3_building_config.bash'), configSetupParams)
+            while not self.processConfigSetup.waitForFinished():
+                QThread.sleep(0.2)
+            self.processConfigSetup.close()
+            # -----------------------------------------------------------------------
+            # EPICS base
+            self.processEpicsBase.start(('%s/%s') % (cloneDir,'e3.bash'), ['base'])
+            while not self.processEpicsBase.waitForFinished():
+                QThread.sleep(0.2)
+            self.processEpicsBase.close()
+            # -----------------------------------------------------------------------
+            # Require
+            self.processRequire.start(('%s/%s') % (cloneDir,'e3.bash'), ['req'])
+            while not self.processRequire.waitForFinished():
+                QThread.sleep(0.2)
+            self.processRequire.close()
+            # -----------------------------------------------------------------------
+            # Module groups
+            #   e.g.:
+            #       ./e3.bash -cta4 mod
+            #       ./e3.bash -ctao mod
+            self.processModules.start(('%s/%s') % (cloneDir,'e3.bash'), [self.e3Modules, 'mod'])
+            while not self.processModules.waitForFinished():
+                QThread.sleep(0.2)
+            self.processModules.close()
+            # -----------------------------------------------------------------------
+            # Check the installation
+            # -----------------------------------------------------------------------
+            self.statusBar.showMessage('Installation procedures concluded!', 15000)
+            # saving all the log into a file
+            logFile = open(os.path.join(cloneDir, 'installation_log_%s.txt' % time.strftime('%d%h%Y-%Hh%Mm%Ss')), 'w')
+            logFile.write(self.textLog.toPlainText())
+            logFile.close()
+
+        except:
+            self.statusBar.showMessage('An exception occurred... ask for support!', 15000)
+            self.pushInstall.setEnabled(True)
+            return 1
+
+        return 0
+
+
+    """
+    generic
+    """
+    def showProcessResults(self, process):
+        self.textLog.append(str(process.readAllStandardOutput(), 'utf-8'))
+
+    def showProcessErrors(self, process):
+        self.statusBar.showMessage(str(process.readAllStandardError(), 'utf-8'), 15000)
+
+
+    """
+    specific
+    """
+    def startedGitClone(self):
         # cloning the e3 repo
         self.textLog.append('---\nCloning the e3 repo...\n')
-        if not os.path.exists(self.localDir):
-            os.makedirs(self.localDir)
-        self.process.start('git', ['clone', self.defaultRepo, cloneDir])
-        # -----------------------------------------------------------------------
-        # configure the e3 environment settings
-        self.textLog.append('---\nConfigure the e3 environment settings...\n')
-        if not os.path.exists(self.targetDir):
-            os.makedirs(self.targetDir)
-        self.process.start(('%s/%s') % (cloneDir,'e3_building_config.bash'), ['-b %s' % self.baseVersion.replace('R',''), '-t %s' % self.targetDir, 'setup'])
-
-
-    def showProcessResults(self):
-        self.textLog.append(str(self.process.readAll(), 'utf-8'))
-
-
-    def disableInstallButton(self):
         self.pushInstall.setEnabled(False)
 
+    def finishedGitClone(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nCloning step has been concluded with exitCode: %d' % self.processGitClone.exitCode())
 
-    def processFinished(self, exitCode, exitStatus):
-        self.textLog.append('\nThis step has been concluded with exitCode: %d\n---' % exitCode)
+    def startedConfigSetup(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nConfigure the e3 environment settings...\n')
+
+    def finishedConfigSetup(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nConfiguration step has been concluded with exitCode: %d' % self.processConfigSetup.exitCode())
+
+    def startedEpicsBase(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nInstall EPICS Base...\n')
+
+    def finishedEpicsBase(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nEPCIS base installation step has been concluded with exitCode: %d' % self.processConfigSetup.exitCode())
+
+    def startedRequire(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nInstall Require...\n')
+
+    def finishedRequire(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nRequire installation step has been concluded with exitCode: %d' % self.processConfigSetup.exitCode())
+        self.pushInstall.setEnabled(True)
+
+    def startedModules(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nInstall Module groups...\n')
+
+    def finishedModules(self):
+        # cloning the e3 repo
+        self.textLog.append('---\nModule groups installation step has been concluded with exitCode: %d' % self.processConfigSetup.exitCode())
         self.pushInstall.setEnabled(True)
 
 
@@ -183,6 +286,7 @@ class e3InstallerWindow(QMainWindow):
         self.defaultTargetDir   = None
         self.localDir           = None
         self.targetDir          = None
+        self.e3Modules          = None
         # ---------------------------------------------------------------------
         # integer/float
         pass
@@ -200,9 +304,24 @@ class e3InstallerWindow(QMainWindow):
             'IFC Module Group':         ModuleGroups.IFCGRP,
             'Area Detector Group':      ModuleGroups.ADRGRP,
             'LLRF Group':               ModuleGroups.LRFGRP }
+
+        self.e3ModulesParamDict = {
+            ModuleGroups.CMNGRP: 'c',
+            ModuleGroups.TMGGRP: 't',
+            ModuleGroups.EV4GRP: '4',
+            ModuleGroups.ECMGRP: 'e',
+            ModuleGroups.PSIGRP: 'i',
+            ModuleGroups.IFCGRP: 'f',
+            ModuleGroups.ADRGRP: 'a',
+            ModuleGroups.LRFGRP: 'l'
+        }
         # ---------------------------------------------------------------------
         # Qt types
-        self.process = QProcess(self)
+        self.processGitClone    = QProcess(self)
+        self.processConfigSetup = QProcess(self)
+        self.processEpicsBase   = QProcess(self)
+        self.processRequire     = QProcess(self)
+        self.processModules     = QProcess(self)
 
         # ---------------------------------------------------------------------
         # loading the main form
@@ -219,13 +338,28 @@ class e3InstallerWindow(QMainWindow):
         self.pushLocalDir.clicked.connect(self.pushLocalDirClicked)
         self.pushTargetDir.clicked.connect(self.pushTargetDirClicked)
         self.pushInstall.clicked.connect(self.pushInstallClicked)
+        self.pushClearLog.clicked.connect(lambda: self.textLog.setText(''))
         self.actionRepo.triggered.connect(self.menuActionRepo)
-        #--
-        self.process.readyReadStandardOutput.connect(self.showProcessResults)
-        #self.process.readyRead.connect(self.showProcessResults)
-       # Disable the install button when process starts, and enable it when it finishes
-        self.process.started.connect(lambda: self.pushInstall.setEnabled(False))
-        self.process.finished.connect(self.processFinished)
+        self.processGitClone.readyReadStandardOutput.connect(partial(self.showProcessResults, self.processGitClone))
+        self.processGitClone.readyReadStandardError.connect(partial(self.showProcessErrors, self.processGitClone))
+        self.processConfigSetup.readyReadStandardOutput.connect(partial(self.showProcessResults, self.processConfigSetup))
+        self.processConfigSetup.readyReadStandardError.connect(partial(self.showProcessErrors, self.processConfigSetup))
+        self.processEpicsBase.readyReadStandardOutput.connect(partial(self.showProcessResults, self.processEpicsBase))
+        self.processEpicsBase.readyReadStandardError.connect(partial(self.showProcessErrors, self.processEpicsBase))
+        self.processRequire.readyReadStandardOutput.connect(partial(self.showProcessResults, self.processRequire))
+        self.processRequire.readyReadStandardError.connect(partial(self.showProcessErrors, self.processRequire))
+        self.processModules.readyReadStandardOutput.connect(partial(self.showProcessResults, self.processModules))
+        self.processModules.readyReadStandardError.connect(partial(self.showProcessErrors, self.processModules))
+        self.processGitClone.started.connect(self.startedGitClone)
+        self.processGitClone.finished.connect(self.finishedGitClone)
+        self.processConfigSetup.started.connect(self.startedConfigSetup)
+        self.processConfigSetup.finished.connect(self.finishedConfigSetup)
+        self.processEpicsBase.started.connect(self.startedEpicsBase)
+        self.processEpicsBase.finished.connect(self.finishedEpicsBase)
+        self.processRequire.started.connect(self.startedRequire)
+        self.processRequire.finished.connect(self.finishedRequire)
+        self.processModules.started.connect(self.startedModules)
+        self.processModules.finished.connect(self.finishedModules)
         # ---------------------------------------------------------------------
         # connecting slots and form components
         self.tabInstallSteps.currentChanged.connect(self.updateSelectedTab)
@@ -235,7 +369,7 @@ class e3InstallerWindow(QMainWindow):
         self.lstModules.itemSelectionChanged.connect(self.updateSelectedModules)
         # ---------------------------------------------------------------------
         # configuring envent filter for tab
-        #self.tabInstallSteps.tabBar().installEventFilter(self)
+        self.tabInstallSteps.tabBar().installEventFilter(self)
 
 
     @pyqtSlot(int)
@@ -247,7 +381,7 @@ class e3InstallerWindow(QMainWindow):
     def updateAgreementAcceptance(self, val):
         # 
         self.agrementAccepted = self.chkAgree.isChecked()
-        # print("license accepted: " + str(self.agrementAccepted))
+        self.statusBar.showMessage('License accepted: ' + str(self.agrementAccepted), 15000)
         self.pushNext.setEnabled(self.agrementAccepted)
 
 
@@ -284,9 +418,9 @@ class e3InstallerWindow(QMainWindow):
 
     def initialDirPlaces(self):
         self.defaultLocalDir    = str(Path.home())
-        self.defaultTargetDir   = '/opt/'
-        self.localDir           = str(Path.home()) + '/e3/'
-        self.targetDir          = '/opt/epics/'
+        self.defaultTargetDir   = '/opt'
+        self.localDir           = (str(Path.home()) + '/e3').replace('//', '/')
+        self.targetDir          = '/opt/epics'
 
 
     def eventFilter(self, object, event): 
@@ -333,9 +467,6 @@ class e3InstallerWindow(QMainWindow):
                         for tag in returnedTags.json():
                             tagsList.append(tag['name'])
                         self.lstRequire.addItems(tagsList)
-                        # for index in range(self.lstRequire.count()):
-                        #     self.lstRequire.item(index).setCheckState(Qt.Unchecked)
-                        # self.lstRequire.item(0).setCheckState(Qt.Checked)
                         self.lstRequire.item(0).setSelected(True)
                         self.requireVersion = self.lstRequire.item(0).text()
                     else:
@@ -387,18 +518,21 @@ class e3InstallerWindow(QMainWindow):
                         self.lstModules.item(allModulesList.index(displayedCommonName)).setSelected(True)
                     except:
                         self.statusBar.showMessage("Error when trying to set common group at modules list...", 15000)
-            #print(moduleEnumList)
+            # filling up the attribute that will be used when running the Modules installation procedure
+            self.e3Modules = '-'
+            for e3Mod in moduleEnumList:
+                self.e3Modules += str(self.e3ModulesParamDict[e3Mod])
+            if self.chkOnly.isChecked():
+                self.e3Modules += 'o'
+            #print(self.e3Modules)
 
             self.initialDirPlaces()
-
             self.textLocalDir.setText(self.localDir)
             self.textTargetDir.setText(self.targetDir)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    #win = uic.loadUi(os.path.dirname(__file__) + '/../forms/main.ui')
-    #win.show()
     mainWin = e3InstallerWindow()
     mainWin.show()
     # wait until the window is closed...
